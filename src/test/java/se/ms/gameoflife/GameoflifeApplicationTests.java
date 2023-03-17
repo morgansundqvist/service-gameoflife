c48f8fd75cd9694c82c85d374e1a8b3d6e8d9055
Morgan Sundqvist <morgan.sundqvist@gmail.com>
2023-03-17 07:57:21 +0100

    Base template from start.spring.io

---
+++ src/test/java/se/ms/gameoflife/GameoflifeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ms.gameoflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameoflifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
