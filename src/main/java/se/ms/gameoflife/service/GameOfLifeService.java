495f7b39f3127b67daa60f2e74aa98406d6f5aa1
Morgan Sundqvist <morgan.sundqvist@gmail.com>
2023-03-17 09:00:11 +0100

    feat: Solving Underpopulation GameOfLifeService unit test

--- src/main/java/se/ms/gameoflife/service/GameOfLifeService.java
+++ src/main/java/se/ms/gameoflife/service/GameOfLifeService.java
@@ -1,20 +1,50 @@
 package se.ms.gameoflife.service;
 
-import java.util.function.BooleanSupplier;
-
 public class GameOfLifeService {
 
-    public GameOfLifeService(int i, int j) {
+    private boolean[][] board;
+    private int boardHeight;
+    private int boardWidth;
+
+    public GameOfLifeService(int boardHeight, int boardWidth) {
+        this.boardHeight = boardHeight;
+        this.boardWidth = boardWidth;
+
+        this.board = new boolean[this.boardHeight][this.boardWidth];
     }
 
-    public void setCellState(int i, int j, boolean b) {
+    public void setCellState(int row, int column, boolean isAlive) {
+        this.board[row][column] = isAlive;
     }
 
     public void transitionToNextGeneration() {
+        boolean[][] nextGenerationBoard = new boolean[this.boardHeight][this.boardWidth];
+        for (int rowIndex = 0; rowIndex < nextGenerationBoard.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < nextGenerationBoard[rowIndex].length; columnIndex++) {
+                boolean cellIsAlive = this.board[rowIndex][columnIndex];
+                int numberOfAliveNeighborCells = 0;
+                for (int i = rowIndex - 1; i <= rowIndex + 1; i++) {
+                    for (int j = columnIndex - 1; j < columnIndex + 1; j++) {
+                        if (i >= 0 && j >= 0 && i < this.boardHeight && j < this.boardWidth && i != rowIndex
+                                && j != columnIndex) {
+                            if (this.board[i][j]) {
+                                numberOfAliveNeighborCells++;
+                            }
+                        }
+                    }
+
+                }
+                if (cellIsAlive && numberOfAliveNeighborCells < 2) {
+                    nextGenerationBoard[rowIndex][columnIndex] = false;
+                }
+            }
+        }
+
+        this.board = nextGenerationBoard;
     }
 
-    public BooleanSupplier[][] getBoard() {
-        return null;
+    public boolean[][] getBoard() {
+        return this.board;
     }
 
 }
